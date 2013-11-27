3683dd2b13efec70be1d3f74c8880407eb060639
Alberto Polidura <alberto.polidura@clowod.com>
2013-11-27 19:35:54 +0100

    Keyboard is now hidden after search

--- RottenApp/src/main/java/com/example/rottenapp/MainActivity.java
+++ RottenApp/src/main/java/com/example/rottenapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rottenapp;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -74,6 +77,9 @@ public class MainActivity extends ListActivity {
 
     public void searchTitle(View v) {
         EditText title = (EditText) findViewById(R.id.editText);
+        InputMethodManager imm = (InputMethodManager)getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(title.getWindowToken(), 0);
         String input = title.getText().toString();
         new GetData().execute(input);
     }
