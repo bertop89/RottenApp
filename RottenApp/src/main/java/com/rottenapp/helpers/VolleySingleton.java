9011ee926bc051482ed0fd42843b64dbfddd4f37
Alberto Polidura <alberto.polidura@clowod.com>
2014-01-30 17:28:02 +0100

    Using a better LRU Image Memory cache (Trey Robinson)

--- RottenApp/src/main/java/com/rottenapp/helpers/VolleySingleton.java
+++ RottenApp/src/main/java/com/rottenapp/helpers/VolleySingleton.java
@@ -3,30 +3,26 @@ package com.rottenapp.helpers;
 /**
  * Created by Alberto Polidura on 28/11/13.
  */
+
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.support.v4.util.LruCache;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
+import com.rottenapp.data.BitmapLruImageCache;
 
 public class VolleySingleton {
     private static VolleySingleton mInstance = null;
     private RequestQueue mRequestQueue;
     private ImageLoader mImageLoader;
 
+    private static int DISK_IMAGECACHE_SIZE = 1024*1024*10;
+    private ImageLoader.ImageCache mImageCache;
+
     private VolleySingleton(Context context){
         mRequestQueue = Volley.newRequestQueue(context);
-        mImageLoader = new ImageLoader(this.mRequestQueue, new ImageLoader.ImageCache() {
-            private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
-            public void putBitmap(String url, Bitmap bitmap) {
-                mCache.put(url, bitmap);
-            }
-            public Bitmap getBitmap(String url) {
-                return mCache.get(url);
-            }
-        });
+        mImageCache = new BitmapLruImageCache(DISK_IMAGECACHE_SIZE);
+        mImageLoader = new ImageLoader(this.mRequestQueue, mImageCache);
     }
 
     public static VolleySingleton getInstance(Context context){
